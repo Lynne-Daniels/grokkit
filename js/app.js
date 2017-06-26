8c987d883a71d6b3c8674112ff502f90aa696dfc
Lynne Daniels <lynnedaniels@Lynnes-MacBook-Pro.local>
2017-06-26 15:32:07 -0700

    front end working

--- js/app.js
+++ js/app.js
@@ -5,26 +5,24 @@ const leftMenu = document.querySelector('.linky');
 
 
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-    var context = this, args = arguments;
-    var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-    };
+	var timeout;
+	return function() {
+		var context = this, args = arguments; 
+		var later = function() {
+			timeout = null;
+			if (!immediate) { func.apply(context, args); }
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) { func.apply(context, args); }
+	};
 }
 
-function moveLeftMenu(){
-    console.log('moving menu', window.scrollY);
-    leftMenu.style.transform = `translateY(${window.scrollY}px)`
-    console.log(`translateY(${window.scrollY}px)`);
+function moveLeftMenu() {
+	console.log('moving menu', window.scrollY);
+	leftMenu.style.transform = `translateY(${window.scrollY}px)`;
+	console.log(`translateY(${window.scrollY}px)`);
 }
 
 window.addEventListener('scroll', debounce(moveLeftMenu));
-
-
